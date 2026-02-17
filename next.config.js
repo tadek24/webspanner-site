e964fc65fd66da02428f9b5ca2026a3bcc1aa1bd
tadek24 <tadekkw123@gmail.com>
2026-02-17 22:28:48 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export',
-    reactStrictMode: true,
+  output: 'export',
+  reactStrictMode: true,
+  images: {               // <--- DODAJ TO
+    unoptimized: true,    // <--- DODAJ TO
+  },
 };
 
 export default nextConfig;
